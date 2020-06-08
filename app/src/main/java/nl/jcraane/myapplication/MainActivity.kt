c7ef6c6eabfdafc0f6bb5e72c691f8e9a6870f6f
jamiecraane <jcraane@gmail.com>
2020-06-08 17:35:31 +0200

    Add simple code to test OPEN_DOCUMENT from sample app

--- app/src/main/java/nl/jcraane/myapplication/MainActivity.kt
+++ app/src/main/java/nl/jcraane/myapplication/MainActivity.kt
@@ -1,11 +1,57 @@
 package nl.jcraane.myapplication
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openDocument.setOnClickListener {
+            openFile()
+        }
+    }
+
+    fun openFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+        }
+
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Timber.i("onActivityResult")
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_FILE_REQUEST_CODE
+            && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            data?.data?.also { uri ->
+                val contentResolver = applicationContext.contentResolver
+                /*val takeFlags: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                contentResolver.takePersistableUriPermission(uri, takeFlags)*/
+
+                contentResolver.openFileDescriptor(uri, "r")?.let { parcelFileDescriptor ->
+                    val descriptor = parcelFileDescriptor.fileDescriptor
+                    FileInputStream(descriptor).use {
+                        val bytes = it.readBytes()
+                        Toast.makeText(this, String(bytes), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val PICK_FILE_REQUEST_CODE  = 1
     }
 }
